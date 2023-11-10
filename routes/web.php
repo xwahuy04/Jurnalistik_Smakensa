deb01e1c6d6200321a9118b2ffb8bfab6dff1740
xwahuy04 <wahyuhavidbaihaqi@gmail.com>
2023-11-10 21:47:57 +0700

    Add a bootstrap template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('tables', function () {
+    return view('tables');
+});
+
+Route::get('sign-in', function () {
+    return view('sign-in');
+});
+
+Route::get('billing', function () {
+    return view('billing');
+});
